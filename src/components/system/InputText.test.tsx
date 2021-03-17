61ab5dfae3cc6bee8e49576b6234731dbc78f004
Rémi GATTAZ <remi.gattaz@gmail.com>
2021-03-17 23:36:00 +0100

    Add more tests to the InputText component

--- src/components/system/InputText.test.tsx
+++ src/components/system/InputText.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import InputText from './InputText';
 
 describe('InputText', () => {
@@ -7,5 +7,20 @@ describe('InputText', () => {
     render(<InputText name="test" />);
     const inputElement = screen.getByRole('textbox', { name: /test/i });
     expect(inputElement).toBeInTheDocument();
+  });
+
+  test('displays the text that is passed', () => {
+    render(<InputText name="test" text="hello" />);
+    const inputElement = screen.getByRole('textbox', { name: /test/i });
+    expect(inputElement.value).toBe("hello");
+  });
+
+  test('triggers the onChange handler if the value of the input is updated', () => {
+    let handlerValue = undefined;
+    const handlerFn = (newValue: string) => handlerValue = newValue;
+    render(<InputText name="test" text="hello" onChange={handlerFn} />);
+    const inputElement = screen.getByRole('textbox', { name: /test/i });
+    fireEvent.change(inputElement, {target: {value: 'world'}});
+    expect(handlerValue).toBe("world");
   });
 });
