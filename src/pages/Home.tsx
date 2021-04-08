3a235ce621e19055e4fb615688da0bed380d55ed
Rémi GATTAZ <remi.gattaz@gmail.com>
2021-04-08 23:06:38 +0200

    Remove the Calculator component from the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import Calculator from 'components/parts/Calculator';
 
 function Home() {
   return (
     <div className="home">
-      <Calculator></Calculator>
     </div>
   );
 }
