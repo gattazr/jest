c387b8b954776698a47fcbbafc9b1a1aae357925
Rémi GATTAZ <remi.gattaz@gmail.com>
2021-04-28 19:51:24 +0200

    Setup the react store
    The story is still empty at the moment

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+
+const reducer = combineReducers({
+})
+
+const store = configureStore({
+  reducer: reducer
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
