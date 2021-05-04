567e15eccbf70116fbd3cb49b7bacddc78b3b8a6
Rémi GATTAZ <remi.gattaz@gmail.com>
2021-05-04 22:24:00 +0200

    Create a simple pagination of nasa-images
    The pagination lists all available pages and clicking on a number will change
    the current page

--- src/components/parts/nasa-images/Results.tsx
+++ src/components/parts/nasa-images/Results.tsx
@@ -1,14 +1,44 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
+import { search } from 'services/api/nasa-images'
+import { SearchResult } from 'models/nasa-images/search-result';
 import Result from 'components/parts/nasa-images/Result';
+import Pagination from 'components/parts/nasa-images/Pagination';
 import { useAppSelector } from 'redux/hooks'
+import { setSearchResults } from 'redux/reducers/nasa-images'
+import { useAppDispatch } from 'redux/hooks'
 
 interface Props { }
 
 const Results = (props: Props) => {
+  const dispatch = useAppDispatch()
+  const query = useAppSelector((state) => state.nasaImages.query)
   const hits = useAppSelector((state) => state.nasaImages.hits)
+  const currentPage = useAppSelector((state) => state.nasaImages.currentPage)
+  const totalPages = Math.floor(hits / 100) + 1
   const results = useAppSelector((state) => state.nasaImages.results)
+
+  const changePage = function(newPageIndex: number) {
+    // Run a search and sends the result to the redux store
+    search(query, newPageIndex).then((res: SearchResult) => {
+      dispatch(setSearchResults({
+        query: query,
+        hits: res.metadata.total_hits,
+        results: res.items,
+        currentPage: newPageIndex
+      }))
+    });
+  }
+
+  let paginationElm = null
+  if (hits > 100) {
+     paginationElm = (
+       <Pagination currentPage={currentPage}
+                   totalPages={totalPages}
+                   onPageSelected={changePage} />
+     )
+  }
 
   if (hits < 0) {
     // Show nothing if hits is negative because it means the state is still in the initial state
@@ -23,9 +53,7 @@ const Results = (props: Props) => {
     return (
       <div className="nasa-images-search-items">
         <p>Yay ! { hits } hits</p>
-        {
-          hits > 100 ? <div>Pagination Soon</div> : null
-        }
+        { paginationElm }
         <div css={{'display': 'grid',
                    'gridTemplateColumns': 'auto auto auto auto'}}>
           {
